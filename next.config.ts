dc02b20a8f3fd81cbfd1c99699ac5de3e8700fbc
RM3 <roger@studioavante.com>
2025-06-29 14:29:23 +0200

    packs assets refactor

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/pistols/tokens/StarterPack.jpg',
+        destination: '/pistols/tokens/packs/StarterPack.jpg',
+        permanent: true,
+      },
+      {
+        source: '/pistols/tokens/GenesisDuelists5x.png',
+        destination: '/pistols/tokens/packs/GenesisDuelists5x.png',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
