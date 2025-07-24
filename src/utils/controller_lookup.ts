871073b71acf4427f72f2fdd12b54d3da21333f6
RM3 <roger@studioavante.com>
2025-07-24 14:01:52 -0300

    fixed token character name for Imps

--- src/utils/controller_lookup.ts
+++ src/utils/controller_lookup.ts
@@ -1,11 +1,16 @@
 import { BigNumberish } from 'starknet'
 import { lookupAddresses } from '@cartridge/controller'
-import { bigintToHex, capitalize } from '@underware/pistols-sdk/utils'
+import { CHARACTER_NAMES } from '@underware/pistols-sdk/pistols/constants'
+import { bigintEquals, bigintToHex } from '@underware/pistols-sdk/utils'
 
 export async function getControllerUsername(owner: BigNumberish) {
-  const _owner = bigintToHex(owner);
-  const addresses = await lookupAddresses([_owner]);
-  const name = addresses.get(_owner) || '';
-  return name;
-  // return capitalize(name);
+  // check if owner is a character
+  let result = CHARACTER_NAMES[Object.keys(CHARACTER_NAMES).find(key => bigintEquals(owner, key)) ?? ''];
+  // if not, lookup address
+  if (!result ) {
+    const _owner = bigintToHex(owner);
+    const addresses = await lookupAddresses([_owner]);
+    result = addresses.get(_owner) || '';
+  }
+  return result;
 }
